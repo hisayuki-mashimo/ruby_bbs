bc3d0875fcf97fca17b07447c94d16b11eba7520
真下悠之 <hisayuki-mashimo@gyoumuyouPC.local>
2018-01-07 23:37:18 +0900

    アクション生成 追加系

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,8 +1,45 @@
 class ArticleController < ApplicationController
   def list
     limit  = 3
-    offset = (params[:page].to_i - 1) * limit
+    page   = params[:page] ? params[:page].to_i : 1
+    offset = (page - 1) * limit
 
     @articles = Article.offset(offset).limit(limit)
+    @article  = Article.new();
+  end
+
+  def create_input
+    @article = Article.new()
+
+    if params[:article] && params[:article][:parent_id] then
+      @parent_article = Article.find(params[:article][:parent_id])
+    end
+  end
+
+  def create_confirm
+    @article = Article.new()
+    @article.title = params[:article][:title]
+    @article.body  = params[:article][:body]
+
+    if params[:article] && params[:article][:parent_id] then
+      @parent_article = Article.find(params[:article][:parent_id])
+      @article.parent_id = params[:article][:parent_id]
+    end
+  end
+
+  def create_do
+    dto = {
+      :title   => params[:article][:title],
+      :body    => params[:article][:body],
+      :user_id => 1 # 暫定
+    }
+
+    if params[:article][:parent_id] then
+      dto[:parent_id] = params[:article][:parent_id]
+    end
+
+    Article.create! dto
+
+    redirect_to :action => 'list'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  put 'article/create_input'
+
+  put 'article/create_confirm'
+
+  put 'article/create_do'
+
   get 'article/list'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
